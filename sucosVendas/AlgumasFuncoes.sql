6523afae185be0bd3cb1b94979393120d7f96988
João Luna <joaovic135@hotmail.com>
2022-02-08 16:45:11 -0300

    funcoes

---
+++ sucosVendas/AlgumasFuncoes.sql
@@ -0,0 +1,11 @@
+SELECT LTRIM('         Olá') AS RESULTADO;
+
+SELECT RTRIM('Olá          ') AS RESULTADO;
+
+
+SELECT TRIM('      Olá      ') AS RESULTADO;
+
+
+SELECT CONCAT('Olá', ' ', 'Tudo Bem?') AS RESULTADO;
+
+ 
